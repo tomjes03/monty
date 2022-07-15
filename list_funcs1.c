61b59af9a2b757bdd1e3b5847ab0317d6c750f1a
yustrovic9 <yustrovic09@gmail.com>
2022-07-15 06:06:27 -0700

    commit0

--- list_funcs1.c
+++ list_funcs1.c
@@ -97,23 +97,3 @@ temp->next->prev = temp->prev;
 free(temp);
 return (1);
 }
-
-/**
- * get_dnodeint_at_index - gets the nth node of a doubly linked list
- * @head: pointer to the list
- * @index: index of the node to return
- *
- * Return: address of the node, or if it does not exist, NULL
- */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-unsigned int i = 0;
-if (!head)
-return (NULL);
-while (head && i < index)
-{
-head = head->next;
-i++;
-}
-return (head ? head : NULL);
-}
