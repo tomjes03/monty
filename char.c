bb33598e5db2cdd45fd52c5223dea04ed3572c31
yustrovic9 <yustrovic09@gmail.com>
2022-07-17 14:59:36 -0700

    my char.c commit message

--- char.c
+++ char.c
@@ -6,6 +6,7 @@
  * @stack: double pointer to the stack to push to
  * @line_number: number of the line in the file
  */
+
 void pchar_handler(stack_t **stack, unsigned int line_number)
 {
 stack_t *node = *stack;
@@ -30,6 +31,7 @@ putchar('\n');
  * @stack: double pointer to the stack to push to
  * @line_number: number of the line in the file
  */
+
 void pstr_handler(stack_t **stack, unsigned int line_number)
 {
 stack_t *node = *stack;
